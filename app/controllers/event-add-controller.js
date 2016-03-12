7cb45295506f2f82d57328783a912e84d836e375
Alan Pereira Mortoni <mortoni.alan@hotmail.com>
2016-03-12 16:23:46 +1000

    fix: problems with form were solved

--- app/controllers/event-add-controller.js
+++ app/controllers/event-add-controller.js
@@ -1,66 +1,69 @@
 (function () {
-    'use strict';
-    angular.module('meetUp').controller('EventAddCtrl', EventAddCtrl);
+  'use strict';
+  angular.module('meetUp').controller('EventAddCtrl', EventAddCtrl);
 
-    EventAddCtrl.$inject = ['$scope', 'APP_SETTINGS', '$firebaseArray', '$rootScope', '$location'];
+  EventAddCtrl.$inject = ['$scope', 'APP_SETTINGS', '$firebaseArray', '$rootScope', '$location'];
 
-    function EventAddCtrl($scope, APP_SETTINGS, $firebaseArray, $rootScope, $location) {
-        var ref = new Firebase(APP_SETTINGS.FIREBASE_URL + '/events');
-        $scope.event = {};
+  function EventAddCtrl($scope, APP_SETTINGS, $firebaseArray, $rootScope, $location) {
+    var ref = new Firebase(APP_SETTINGS.FIREBASE_URL + '/events');
+    $scope.event = {};
 
-        $scope.doCreate = function(){
+    $scope.doCreate = function(){
 
-            if(typeof $scope.event.info === 'undefined')
-                $scope.event.info = '';
+      if(typeof $scope.event.info === 'undefined')
+      $scope.event.info = '';
 
-        	var event = {
-        		id: $rootScope.user.password.email,
-	        	name: $scope.event.name,
-	        	type: $scope.event.type,
-	        	host: $scope.event.host,
-	        	start_date: getFormattedDate($scope.event.startDate),
-	        	end_date: getFormattedDate($scope.event.endDate),
-	        	guest_list: $scope.event.guest,
-	        	location: $scope.event.location,
-	        	aditional_info: $scope.event.info
-	        };
+      var event = {
+        id: $rootScope.user.password.email,
+        name: $scope.event.name,
+        type: $scope.event.type,
+        host: $scope.event.host,
+        start_date: getFormattedDate($scope.event.startDate),
+        end_date: getFormattedDate($scope.event.endDate),
+        guest_list: $scope.event.guest,
+        location: $scope.event.location,
+        aditional_info: $scope.event.info
+      };
 
-        	ref.push(event);
-          $rootScope.event_qdt++;
+      ref.push(event);
+      $rootScope.event_qdt++;
 
-          $location.path('/home');
+      $location.path('/home');
 
-        }
-        Number.prototype.padLeft = function(base,chr){
-           var  len = (String(base || 10).length - String(this).length)+1;
-           return len > 0? new Array(len).join(chr || '0')+this : this;
-        }
-
-        function getFormattedDate(date) {
-            var str = date.getDate().padLeft() + "/" + ((date.getMonth() +1 ).padLeft()) + "/" + date.getFullYear() + " " +  date.getHours() + ":" + date.getMinutes();
-            return str;
-        }
-
-        $scope.checkDate = function(){
-            if(!(typeof $scope.event.startDate === 'undefined') || !(typeof $scope.event.endDate === 'undefined'))
-                if(new Date($scope.event.startDate) > new Date($scope.event.endDate))
-                    return false;
-
-            return true;
-        }
-
-        $scope.DateChanged = function(){
-            if(!(typeof $scope.event.startDate === 'undefined') || !(typeof $scope.event.endDate === 'undefined'))
-                if(new Date($scope.event.startDate) > new Date($scope.event.endDate)){
-                    $scope.eventError =  true;
-                    $scope.addEvent.startDate.$invalid = true;
-                    $scope.addEvent.endDate.$invalid = true;
-                    $scope.eventErrorMessage = 'End Date must be higher than Start Date!'
-                }else{
-                    $scope.eventError =  false;
-                    $scope.addEvent.startDate.$invalid = false;
-                    $scope.addEvent.endDate.$invalid = false;
-                }
-        }
     }
+    Number.prototype.padLeft = function(base,chr){
+      var  len = (String(base || 10).length - String(this).length)+1;
+      return len > 0? new Array(len).join(chr || '0')+this : this;
+    }
+
+    function getFormattedDate(date) {
+      var str = date.getDate().padLeft() + "/" + ((date.getMonth() +1 ).padLeft()) + "/" + date.getFullYear() + " " +  date.getHours() + ":" + date.getMinutes();
+      return str;
+    }
+
+    $scope.checkDate = function(){
+      if(!(typeof $scope.event.startDate === 'undefined') || !(typeof $scope.event.endDate === 'undefined'))
+      if(new Date($scope.event.startDate) > new Date($scope.event.endDate))
+      return false;
+
+      return true;
+    }
+
+    $scope.DateChanged = function(){
+      if(!(typeof $scope.event.startDate === 'undefined') || !(typeof $scope.event.endDate === 'undefined'))
+      if(new Date($scope.event.startDate) > new Date($scope.event.endDate)){
+        $scope.eventError =  true;
+        $scope.addEvent.startDate.$invalid = true;
+        $scope.addEvent.endDate.$invalid = true;
+        $scope.eventErrorMessage = 'End Date must be higher than Start Date!'
+      }else{
+        $scope.eventError =  false;
+        $scope.addEvent.startDate.$invalid = false;
+        $scope.addEvent.endDate.$invalid = false;
+      }
+    }
+
+    $("#event_name").focus();
+
+  }
 })();
--- app/controllers/login-controller.js
+++ app/controllers/login-controller.js
@@ -1,98 +1,103 @@
 (function() {
-    'use strict';
-    angular.module('meetUp').controller('LoginCtrl', LoginCtrl);
+  'use strict';
+  angular.module('meetUp').controller('LoginCtrl', LoginCtrl);
 
-    LoginCtrl.$inject = ['$scope', '$rootScope', '$location', 'APP_SETTINGS', '$firebaseAuth', '$firebaseArray'];
+  LoginCtrl.$inject = ['$scope', '$rootScope', '$location', 'APP_SETTINGS', '$firebaseAuth'];
 
-    function LoginCtrl($scope, $rootScope, $location, APP_SETTINGS, $firebaseAuth, $firebaseArray) {
-        var ref = new Firebase(APP_SETTINGS.FIREBASE_URL);
-        var ref2 = new Firebase(APP_SETTINGS.FIREBASE_URL + '/users');
-        var auth = $firebaseAuth(ref);
-        $rootScope.user = null;
-        $scope.createdUser = false;
+  function LoginCtrl($scope, $rootScope, $location, APP_SETTINGS, $firebaseAuth) {
+    var ref = new Firebase(APP_SETTINGS.FIREBASE_URL);
+    var ref2 = new Firebase(APP_SETTINGS.FIREBASE_URL + '/users');
+    var auth = $firebaseAuth(ref);
+    $rootScope.user = null;
+    $scope.createdUser = false;
 
-        var test = $rootScope.events;
+    $scope.logout = function(){
+      $rootScope.user = null;
+      $location.path('/login');
+    }
 
-        $scope.logout = function(){
-           $rootScope.user = null;
-           $location.path('/login');
-        }
-        $scope.doLogin = function(){
-            var user = [{
-                name: '',
-                email: $scope.user.email,
-                password: $scope.user.password
-            }];
+    $scope.doLogin = function(){
+      var user = [{
+        name: '',
+        email: $scope.user.email,
+        password: $scope.user.password
+      }];
 
-            auth.$authWithPassword({
-                email: $scope.user.email,
-                password: $scope.user.password
-            }).then(function(user) {
-                    //Success callback
-                    $rootScope.event_qdt = 0;
-                    $rootScope.user = user;
+      auth.$authWithPassword({
+        email: $scope.user.email,
+        password: $scope.user.password
+      }).then(function(user) {
+        //Success callback
+        $rootScope.event_qdt = 0;
+        $rootScope.user = user;
 
-                    $rootScope.events.forEach(function(event) {
-                        if(event.id == $scope.user.email)
-                            $rootScope.event_qdt++;
-                    });
-
-                    $location.path('/home');
-                    console.log('Authentication successful');
-                }, function(error) {
-                    //Failure callback
-                    $scope.signError = true;
-                    $scope.signErrorMessage = error.message;
-                    console.log('Authentication failure');
-
-                });
-        }
-
-        $scope.doRegister = function(){
-            var user = {
-                name: $scope.guest.name,
-                email: $scope.guest.email,
-                password: $scope.guest.password
-            };
-
-            if ($scope.guest.email && $scope.guest.password) {
-
-                ref.createUser({
-                  email    : $scope.guest.email,
-                  password : $scope.guest.password
-                }, function(error, userData) {
-                  if (error) {
-                    console.log("Error creating user:", error);
-                  } else {
-                    ref2.push(user);
-                    document.getElementById("flipper__checkbox").checked = false;
-                    console.log("Successfully created user account with uid:", userData.uid);
-                  }
-                });
-            }
-        };
-
-        $scope.change = function() {
-            if($scope.guest.password != $scope.guest.password_confirm){
-                $scope.regForm.password_confirm.$invalid = true;
-                $scope.regErrorMessage = 'passwords do not match';
-                $scope.regError = true;
-            }else{
-                $scope.regForm.password_confirm.$invalid = false;
-                $scope.regError = false;
-            }
-        };
-
-        $scope.changePassword = function() {
-            if(!(typeof $scope.guest.password_confirm === 'undefined'))
-               $scope.change();
-
-        };
-
-        $('.nav a').on('click', function(){
-            $('.navbar-toggle').click() //bootstrap 3.x by Richard
+        $rootScope.events.forEach(function(event) {
+          if(event.id == $scope.user.email)
+          $rootScope.event_qdt++;
         });
 
+        $location.path('/home');
+        console.log('Authentication successful');
+      }, function(error) {
+        //Failure callback
+        $scope.signError = true;
+        $scope.signErrorMessage = error.message;
+        console.log('Authentication failure');
 
+      });
     }
+
+    $scope.doRegister = function(){
+      var user = {
+        name: $scope.guest.name,
+        job_title: $scope.guest.job_title,
+        email: $scope.guest.email,
+        password: $scope.guest.password
+      };
+
+      if ($scope.guest.email && $scope.guest.password) {
+        auth.$createUser({
+          email: $scope.guest.email,
+          password: $scope.guest.password
+        }).then(function(userData) {
+          $scope.createdUser = true;
+          $scope.guest = null;
+          document.getElementById("flipper__checkbox").checked = false;
+          ref2.push(user);
+          console.log("Successfully created user account with uid:", userData.uid);
+        }).catch(function(error) {
+          console.log("Error creating user:", error);
+        });
+      }
+    };
+
+    $scope.change = function() {
+      if($scope.guest.password != $scope.guest.password_confirm){
+        $scope.regForm.password_confirm.$invalid = true;
+        $scope.regErrorMessage = 'passwords do not match';
+        $scope.regError = true;
+      }else{
+        $scope.regForm.password_confirm.$invalid = false;
+        $scope.regError = false;
+      }
+    };
+
+    $scope.changePassword = function() {
+      if(!(typeof $scope.guest.password_confirm === 'undefined'))
+      $scope.change();
+
+    };
+
+    $scope.logFocus = function(){
+      $("#log_email").focus();
+    }
+
+    $scope.regFocus = function(){
+      $("#reg_name").focus();
+    }
+
+    $('.nav a').on('click', function(){
+      $('.navbar-toggle').click()
+    });
+  }
 })();
